41f03bb75cb75faff446a7bfb5be4344a38c1dff
Alexander <kir-al93@yandex.ru>
2022-04-23 19:24:13 +0900

    Fix main.py

--- coloredmanga_scrapper_async/main.py
+++ coloredmanga_scrapper_async/main.py
@@ -4,24 +4,24 @@ from utility import utils
 def main():
     # файл со структурой манги, куда будет произведено сохранение структуры, и впоследствии из которого может быть
     # осуществлена её загрузка.
-    file_contents = "..\\coloredmanga_scrapper_async\\data\\contents_shrink.json"
+    file_contents = "..\\coloredmanga_scrapper_async\\data\\contents_full.json"
     # Корневая директория, в которой будет создана папка с названием манги и загружены её тома, главы и страницы.
     dir_root = "D:\\"
 
     # Ссылка на главную страницу манги.
-    url = "https://coloredmanga.com/manga/vagabond/"
+    url = "https://coloredmanga.com/manga/one-piece/"
 
     # Загрузка структуры манги с сайта.
-    manga_contents = utils.build_contents(url)
+    # manga_contents = utils.build_contents(url)
 
     # Сохранение загруженной структуры в файле формата JSON.
-    utils.save_contents(manga_contents, filename=file_contents)
+    # utils.save_contents(manga_contents, filename=file_contents)
 
     # Загрузка структуры манги из файла.
-    # manga_contents = utils.build_contents(file_contents, from_file=True)
+    manga_contents = utils.build_contents(file_contents, from_file=True)
 
     # Скачивание манги.
-    utils.download_manga(manga_contents, dir_root, start_with=0, end_with=0, is_flatten=False)
+    utils.download_manga(manga_contents, dir_root, start_with=94, end_with=95, is_flatten=True)
 
 
 if __name__ == '__main__':
